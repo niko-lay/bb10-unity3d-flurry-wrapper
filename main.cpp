98b674acc0677e3bea709f0296bb12358c11e602
Nikolay <djanmail+github@gmail.com>
2014-07-31 03:19:07 +0300

    + event params

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include "FlurryC.h"
+#include <string.h>
 
 
 extern "C"
@@ -29,6 +30,22 @@ extern "C"
 
 	void flurry_log_event_Ext(const char *anEventName, int aTimed ){
 		flurry_log_event(anEventName, aTimed);
+	}
+
+	void flurry_log_event_with_params_Ext(const char *anEventName, int aTimed, const char *aParamsAndKeys, int aParamCount){
+	    //flurry_log_event_with_params(const char *anEventName, int aTimed, const char **aParamKeys, const char **aParamValues, int aParamCount);
+	    char * str1 = new char [strlen(aParamsAndKeys)];
+	    strcpy(str1, aParamsAndKeys);
+
+	    char * pch = strtok (str1,"\n");
+	    while (pch != NULL)                         // пока есть лексемы
+	    {
+	        fprintf(stderr, pch);
+	        pch = strtok (NULL, "\n");
+	    }
+
+	    delete [] str1;
+
 	}
 
 	void flurry_end_timed_event_Ext(const char *anEventName){
